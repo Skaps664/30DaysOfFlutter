9ba2c0dd18aec52cc526388af800407645a8c8e8
SKPAS664 <msudaisk664@gmail.com>
2021-09-24 01:52:53 +0500

    day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 
-class LoginPage  extends StatelessWidget {
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-        style: TextStyle(
-          color: Colors.blue,
-          fontWeight: FontWeight.bold
-          ),
-          ),
-        ),
-      );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hi Skaps");
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.purple.shade700,
+                      )),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
