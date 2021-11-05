dd19f241a08247b803f8845034e67d40b1dee0ee
Skaps664 <msudaisk664@gmail.com>
2021-11-05 15:12:00 +0500

    day12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,16 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "Apple Iphone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://images.macrumors.com/article-new/2020/10/iphone12prodesignback.jpg"),
+  ];
+}
+
 class Item {
   final int id;
   final String name;
@@ -14,14 +27,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-      id: 1,
-      name: "Iphone 12 Pro",
-      desc: "Apple Iphone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FApple-iPhone-Pacific-Carrier-Subscription%2Fdp%2FB08L5Q4BLG&psig=AOvVaw1iRzEk0OXFfTTfkLSMvmw5&ust=1635586873738000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCmnYmq7_MCFQAAAAAdAAAAABAD"),
-];
